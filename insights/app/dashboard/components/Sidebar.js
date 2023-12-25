7c13e51db6ef662b83df819111aa5e950a9a9988
Subodh Khanal <skhanal5@terpmail.umd.edu>
2023-12-25 14:38:32 -0500

    reverted changes

--- insights/app/dashboard/components/Sidebar.js
+++ insights/app/dashboard/components/Sidebar.js
@@ -14,30 +14,31 @@ export default function SideBar() {
     {
       name: "Dashboard",
       href: "/dashboard",
-      icon: <ChartBarSquareIcon className="w-full md:w-1/6 lg:w-1/12" />,
+      icon: <ChartBarSquareIcon className="w-2/6 md:w-1/6 lg:w-2/12" />,
     },
     {
       name: "Tracker",
       href: "/dashboard/tracker",
-      icon: <ClipboardDocumentListIcon className="w-full md:w-1/6 lg:w-1/12" />,
+      icon: <ClipboardDocumentListIcon className="w-2/6 md:w-1/6 lg:w-2/12" />,
     },
     {
       name: "Goals",
       href: "/dashboard/goals",
-      icon: <QueueListIcon className="w-full md:w-1/6 lg:w-1/12" />,
+      icon: <QueueListIcon className="w-2/6 md:w-1/6 lg:w-2/12" />,
     },
     {
       name: "Metrics",
       href: "/dashboard/metrics",
-      icon: <ChartPieIcon className="w-full md:w-1/6 lg:w-1/12" />,
+      icon: <ChartPieIcon className="w-2/6 md:w-1/6 lg:w-2/12" />,
     },
   ];
 
   return (
-    <div className="p-5 w-1/6 lg:w-2/12 border-r border-gray-300">
-      <ul className="flex flex-col gap-3 yt">
+    <div className="w-1/6 md:p-10 lg:w-2/12 border-r border-gray-300">
+      <ul className="mt-10 flex flex-col gap-5 justify-center items-start">
+        <div className="hidden md:flex items-center text-sm text-gray-300">Menu</div>
         {paths.map((path, pathIndex) => (
-          <li className="text-base">
+          <li key={pathIndex}>
             <SidebarCell
               key={pathIndex}
               icon={path.icon}
--- insights/app/dashboard/components/SidebarCell.js
+++ insights/app/dashboard/components/SidebarCell.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function SidebarCell({ icon, href, name }) {
   return (
     <Link
-      className="flex flex-row gap-2 justify-start "
+      className="my-0 mx-auto flex flex-row gap-2 justify-center items-center"
       href={href}
     >
       {icon}
-      <div className="hidden md:flex">{name}</div>
+      <div className="hidden md:flex md:text-sm lg:text-base">{name}</div>
     </Link>
   );
 }
