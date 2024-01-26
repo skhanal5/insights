e5dab8158b4da95de5e0235a2fe37048e49820f9
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-01-25 23:46:57 -0500

    Added calendar to dashboard

--- frontend/app/(main)/dashboard/components/DashboardContainer.jsx
+++ frontend/app/(main)/dashboard/components/DashboardContainer.jsx
@@ -1,8 +1,18 @@
-import StatsPanel from "./StatsPanel"
+"use client"
+import StatsPanel from "./StatsPanel";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
 export default function DashboardContainer() {
-    return (
-        <div className="flex flex-col gap-5">
-            <StatsPanel></StatsPanel>
-        </div>
-    )
+  const [date, setDate] = useState(new Date());
+  return (
+    <div className="flex flex-col gap-5">
+      <StatsPanel></StatsPanel>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-lg shadow hover:shadow-lg w-fit border bg-white p-5"
+      />
+    </div>
+  );
 }
