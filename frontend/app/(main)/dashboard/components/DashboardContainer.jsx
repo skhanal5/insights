823dbd31874dc2afb8491fcf8c53ef41155ca4d2
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-01-27 23:04:22 -0500

    Added chart and its dependencies

--- frontend/app/(main)/dashboard/components/DashboardContainer.jsx
+++ frontend/app/(main)/dashboard/components/DashboardContainer.jsx
@@ -2,11 +2,13 @@
 import StatsPanel from "./StatsPanel";
 import { useState } from "react";
 import GoalsPanel from "./GoalsPanel";
+import LineChart from "./LineChart";
 export default function DashboardContainer() {
   const [date, setDate] = useState(new Date());
   return (
     <div className="flex flex-col gap-5">
       <StatsPanel></StatsPanel>
+      <LineChart></LineChart>
       <GoalsPanel></GoalsPanel>
     </div>
   );
--- frontend/app/(main)/dashboard/components/GoalsCell.jsx
+++ frontend/app/(main)/dashboard/components/GoalsCell.jsx
@@ -6,7 +6,7 @@ import {
 } from "@heroicons/react/24/solid";
 
 export default function GoalsCell({ type, value }) {
-  const iconStyle = "h-4 md:h-5 items-center text-slate-500/80";
+  const iconStyle = "h-4 md:h-5 items-center text-white";
 
   //is this duplicity & is this a good practice?
   const getFormattedTitle = () => {
@@ -22,11 +22,11 @@ export default function GoalsCell({ type, value }) {
   const getBgColorFromType = () => {
     switch (type) {
       case "new_goals":
-        return "bg-gradient-to-r from-sky-200 to-sky-300";
+        return "bg-gradient-to-r from-sky-400 to-sky-400/50";
       case "in_progress":
-        return "bg-gradient-to-r from-yellow-200 to-yellow-300";
+        return "bg-gradient-to-r from-blue-400 to-blue-400/50";
       case "completed":
-        return "bg-gradient-to-r from-green-200 to-green-300";
+        return "bg-gradient-to-r from-indigo-400 to-indigo-400/50";
     }
   };
   const getIconFromType = () => {
@@ -46,14 +46,14 @@ export default function GoalsCell({ type, value }) {
 
   return (
     <div
-      className={`w-1/2 h-56 p-5 ${getBgColorFromType()} shadow focus:shadow-lg rounded-lg flex flex-col justify-between`}
+      className={`w-1/6 h-1/3 p-5 ${getBgColorFromType()} shadow focus:shadow-lg rounded-lg flex flex-col justify-between`}
     >
       <div className="flex flex-row gap-5 items-center justify-between">
         <div className="flex flex-row gap-2 items-center">
           <div className="rounded-full p-2 bg-slate-50/40">
             {getIconFromType()}
           </div>
-          <span className="font-semibold text-lg">
+          <span className="text-white font-semibold text-lg">
             {getFormattedTitle()}
           </span>
         </div>
@@ -61,7 +61,7 @@ export default function GoalsCell({ type, value }) {
           <EllipsisVerticalIcon className="w-5 rounded-lg hover:cursor-pointer hover:bg-slate-50/10"></EllipsisVerticalIcon>
         </span>
       </div>
-      <span className="font-semibold">{value}</span>
+      <span className="text-white font-semibold">{value}</span>
     </div>
   );
 }
