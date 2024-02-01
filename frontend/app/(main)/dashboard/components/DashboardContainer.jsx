370d6bbc7081c09e23cf15d99f501fd4515e317e
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-02-01 00:24:46 -0500

    fixed desktop responsiveness

--- frontend/app/(main)/dashboard/components/DashboardContainer.jsx
+++ frontend/app/(main)/dashboard/components/DashboardContainer.jsx
@@ -8,16 +8,16 @@ import ResourcesPanel from "./ResourcesPanel";
 export default function DashboardContainer() {
   const [date, setDate] = useState(new Date());
   return (
-    <div className="flex flex-col gap-5 h-full max-h-full w-full">
+    <div className="flex flex-col lg:flex-row gap-5 h-full max-h-full w-full">
       <div className="flex flex-col gap-5 lg:w-2/3 w-full">
         <div className="flex flex-col lg:h-1/2 gap-5">
           <StatsPanel></StatsPanel>
-          <div className="flex flex-col gap-5 w-full h-full">
+          <div className="flex flex-col lg:flex-row gap-5 w-full h-full">
             <LineChart></LineChart>
             <ResourcesPanel></ResourcesPanel>
           </div>
         </div>
-        <div className="flex flex-row gap-5">
+        <div className="flex flex-row gap-5 lg:w-1/3">
           <RecentApps></RecentApps>
         </div>
       </div>
